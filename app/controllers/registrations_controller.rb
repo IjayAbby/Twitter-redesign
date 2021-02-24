0b86091104e56605ad4ddae2c1ce551a5abe7f73
Ijay Abby <inyangasi4@gmail.com>
2021-02-24 16:12:58 +0300

    add sign up and account update methods

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,14 @@
 class RegistrationsController < ApplicationController
+
+    before_action :sign_up_params, :account_update_params, if: :devise_controller?
+
+    private
+  
+    def sign_up_params
+      params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+    end
+  
+    def account_update_params
+      params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :current_password)
+    end
 end
