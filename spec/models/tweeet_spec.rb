389343794a50a40aedf159ee1516b7e49c99df1e
Ijay Abby <inyangasi4@gmail.com>
2021-03-02 11:37:31 +0300

    add tweeet spec file

---
+++ spec/models/tweeet_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe  Tweeet, type: :model do
+  subject(:tweeet) { build(:tweeet) }
+
+  describe 'associations' do
+    it { is_expected.to belong_to(:user) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:content) }
+
+    it { is_expected.to validate_length_of(:text).is_at_most(255) }
+  end
+end
+
+
