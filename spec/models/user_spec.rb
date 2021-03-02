4ebab33014ca6c41725ae9df8117d725260dd276
Ijay Abby <inyangasi4@gmail.com>
2021-03-02 11:37:42 +0300

    add tweeet spec file

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject(:user) { build(:user) }
+
+  describe 'association' do
+    it { is_expected.to have_many(:tweeets).with_foreign_key(:user_id).dependent(:destroy) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to have_secure_password }
+
+    it { is_expected.to validate_presence_of(:username) }
+
+    it { is_expected.to validate_length_of(:username).is_at_most(255) }
+
+    it { is_expected.to validate_presence_of(:name) }
+
+    it { is_expected.to validate_length_of(:name).is_at_most(20) }
+
+    it { is_expected.to validate_length_of(:password).is_at_least(6) }
+
+    it 'lowercase username before saving' do
+      username = 'helloexamplecom'
+      user.username = username.upcase
+      user.save!
+
+      expect(user.username).to eq(username)
+    end
+
+    it 'strips the username before validation' do
+      username = '   helloexamplecom '
+      user.username = username
+      user.save!
+
+      expect(user.username).to eq('helloexamplecom')
+    end
+
+    it 'generates user auth_token at random' do
+      user.auth_token = nil
+      user.save!
+
+      expect(user.auth_token).to be_present
+    end
+  end
+
+
+end
