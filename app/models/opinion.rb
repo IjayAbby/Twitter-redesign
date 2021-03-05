0d33705a7e78185d6b44af1bebdd5ab2dccdf1fd
Ijay Abby <inyangasi4@gmail.com>
2021-03-05 21:20:38 +0300

    Create Opinion Model

---
+++ app/models/opinion.rb
@@ -0,0 +1,9 @@
+class Opinion < ApplicationRecord
+    validates_presence_of :text
+    validates :text, length: { minimum: 3, maximum: 180 }
+    belongs_to :author, class_name: 'User'
+    has_many :likes, dependent: :destroy
+
+  scope :ordered_by_most_recent, -> { includes(:author).order(created_at: :desc) }
+
+end
