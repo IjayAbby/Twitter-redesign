0639a4a1d4de3652ebd9cdd1a1e59ff385208b91
Ijay Abby <inyangasi4@gmail.com>
2021-03-14 02:17:15 +0300

    Fix errors

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -4,14 +4,7 @@ require "active_support/core_ext/integer/time"
 # test suite. You never need to work with it otherwise. Remember that
 # your test database is "scratch space" for the test suite and is wiped
 # and recreated between test runs. Don't rely on the data there!
-
 Rails.application.configure do
-  config.after_initialize do
-    Bullet.enable        = true
-    Bullet.bullet_logger = true
-    Bullet.raise         = true # raise an error if n+1 query occurs
-  end
-
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.cache_classes = false
