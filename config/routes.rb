5c6db958bebe260147f3987441232dbf924765da
IjayAbby <inyangasi4@gmail.com>
2021-03-18 12:14:18 +0300

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  root 'home#index'
  
   resources :users, only: %i[new create edit update]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  delete '/logout' => 'sessions#destroy'
+  delete 'logout', to: 'sessions#destroy'
   post 'opinion', to: 'opinions#create'
   post 'follow', to: 'followings#create'
   post 'likes/:opinion_id', to: 'likes#create', as: 'likes'
@@ -17,8 +17,6 @@ Rails.application.routes.draw do
   if Rails.env.production?
     get '404', :to => 'application#page_not_found'
  end
-
-  root 'home#index'
 
 
 end
